699b079f1614c29e34a6051090be221c9af4da1b
杨兵 <yangbing@ipaynow.cn>
2020-06-04 17:05:12 +0800

    nacos config

--- nacos-provider/src/main/java/com/lyoyang/controller/UserController.java
+++ nacos-provider/src/main/java/com/lyoyang/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.lyoyang.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.lyoyang.enums.ResponseEnum;
 import com.lyoyang.response.MvcResponse;
 import com.lyoyang.response.UserInfoResp;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@RefreshScope
 public class UserController {
+
+    @Value(value = "${account:alice}")
+    private String userName;
+
 
     @RequestMapping("/api/getUserInfo")
     MvcResponse<UserInfoResp> getUserInfo() {
@@ -21,7 +29,7 @@ public class UserController {
         userInfoResp.setAge(23);
         userInfoResp.setPhone("13223444323");
         userInfoResp.setEmail("grace@32423.com");
-        userInfoResp.setUserName("grace");
+        userInfoResp.setUserName(userName);
         return new MvcResponse(ResponseEnum.SUCCESS, userInfoResp);
     }
 
