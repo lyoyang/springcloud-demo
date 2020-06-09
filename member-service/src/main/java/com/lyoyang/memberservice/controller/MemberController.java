ec696115965def3de52edabdbdd74c9fa89b5f71
杨兵 <yangbing@ipaynow.cn>
2020-06-09 19:57:45 +0800

    cloud

--- member-service/src/main/java/com/lyoyang/memberservice/controller/MemberController.java
+++ member-service/src/main/java/com/lyoyang/memberservice/controller/MemberController.java
@@ -1,6 +1,8 @@
 package com.lyoyang.memberservice.controller;
 
 import com.lyoyang.dto.MemberRespDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 public class MemberController {
 
+    @Autowired
+    private EurekaClientConfigBean eurekaClientConfigBean;
+
     @RequestMapping(value = "/api/getMembers", produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<MemberRespDto> getMembers() {
         MemberRespDto m1 = MemberRespDto.builder().id("1").name("jim").build();
@@ -18,5 +23,11 @@ public class MemberController {
         MemberRespDto m3 = MemberRespDto.builder().id("3").name("bob").build();
         return Arrays.asList(m1, m2, m3);
     }
+
+    @RequestMapping("/getServiceUrl")
+    public Object getServiceUrl() {
+        return eurekaClientConfigBean.getServiceUrl();
+    }
+
 
 }
